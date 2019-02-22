0a625f18b4fd42fff95ffe177660951d9f1806c1
Samuel Viviescas <samuelviviescas@hotmail.com>
2019-02-22 18:41:27 -0500

    Cambio de firma del main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,7 +6,7 @@ import customExceptions.QuotaEnrollExceedException;
 import model.Course;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		Course myCourse;
 		Scanner sc = new Scanner(System.in);
 		
